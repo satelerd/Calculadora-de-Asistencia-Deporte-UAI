0f2cab911edf035e8f6cf6204d4b4577cb708a52
satelerd <satelerd@gmail.com>
2021-03-28 13:17:04 -0300

    Retoques finales frontend 1.1

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 
 function Card({title, body, space}) {
     return (
-        <div className="card border-dark">
+        <div className="card border-dark shadow">
             <div className="card-body text-center">
                 <h4 className="card-title">{title}</h4>
                 <p className="card-text">
