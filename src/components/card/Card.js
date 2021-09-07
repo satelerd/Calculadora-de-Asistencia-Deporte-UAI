cee4ad3edee7d6be20bf1524f505c331ff68d5a0
Daniel <satelerd@gmail.com>
2021-09-07 16:31:22 -0300

    frontend card adjust

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Card.css'
 
-function Card({title, body, space}) {
+function Card({ title, body, space }) {
     return (
-        <div className="card border-dark shadow">
+        <div className="card">
             <div className="card-body text-center">
                 <div className="card-title">{title}</div>
                 <div className="card-text">
